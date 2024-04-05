bb17e53ca9194030a6392a658a73f44332d4c3c1
Nico P <nicopetkas@gmail.com>
2024-04-04 18:56:25 -0700

    blog push

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
+const commentRoutes = require('./commentRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
-
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
