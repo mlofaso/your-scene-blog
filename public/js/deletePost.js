05bef191c35e22c6cbbbc1c7913fea5ea8a1f8a8
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-04-08 21:09:05 -0700

    fixed delete button

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,1 +1,12 @@
-document.querySelector('.update-btn').addEventListener('click', function () {});
+document.querySelector('.delete-btn').addEventListener('click', function () {
+  fetch(
+    `/api/posts/${document
+      .querySelector('.delete-btn')
+      .getAttribute('data-id')}`,
+    {
+      method: 'DELETE',
+    }
+  )
+    .then((response) => response.json())
+    .then(document.location.replace('/'));
+});
